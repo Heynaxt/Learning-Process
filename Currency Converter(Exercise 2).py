174cb592d46f860b38b52e429cc038185c9bac2c
David <102261183+Heynaxt@users.noreply.github.com>
2022-03-26 23:57:42 +0100

    Add files via upload

---
+++ Currency Converter(Exercise 2).py
@@ -0,0 +1,11 @@
+# Currency Converter GBP To EUR
+# Saturday, March 26, 2022
+
+print("Hola, soy un conversor de divisas, le ayudaré a pasar de Euros a Libra esterlina")
+
+euro = float(input("Por favor introce la cantidad de Libras estarlinas: "))
+# Como veis aquí no he puesto int(input() porque el INT solo es para números enteros, si el usuario intenta convertir
+# por ejemplo 9.5 libras a euros le daria un resultado erróneo, para eso es comando FLOAT()
+# FLOAT() Permite representar un número positivo o negativo con decimales.
+
+print("{} libras ".format(euro)+"equivale a {} euros.".format(euro*1.20))
