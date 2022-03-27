e233723b84a3d9bee0f3806dc430a4e429c0b9e7
David <102261183+Heynaxt@users.noreply.github.com>
2022-03-28 00:15:51 +0200

    Update Currency Converter(Exercise 2).py

--- Currency Converter(Exercise 2).py
+++ Currency Converter(Exercise 2).py
@@ -5,7 +5,7 @@ print("Hola, soy un conversor de divisas, le ayudaré a pasar de Euros a Libra e
 
 euro = float(input("Por favor introce la cantidad de Libras estarlinas: "))
 # Como veis aquí no he puesto int(input() porque el INT solo es para números enteros, si el usuario intenta convertir
-# por ejemplo 9.5 libras a euros le daría un resultado erróneo, para eso es comando FLOAT()
+# por ejemplo 9.5 libras a euros le daría un resultado erróneo, para eso es comando FLOAT().
 # FLOAT() Permite representar un número positivo o negativo con decimales.
 
 print("{} libras ".format(euro)+"equivale a {} euros.".format(euro*1.20))
